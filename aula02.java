cc821ab9293e49155acce025ce803142f800619a
daniel@gmail.com <daniel@gmail.com>
2023-12-07 20:54:15 -0300

    add cachorro

--- aula02.java
+++ aula02.java
@@ -2,12 +2,21 @@ public class aula02 {
     public static void main (String[] args) {
         Pernilongo jorge = new Pernilongo();
         jorge.atrapalharSono(); 
+        Cachorro cleyton = new Cachorro();
+        cleyton.atrapalhaSono();
     }
 }
 
 class Pernilongo {
     String onomatopeia = "píííííí";
     public void atrapalharSono() {
+        System.out.println(this.onomatopeia);
+    }
+}
+
+class Cachorro {
+    String onomatopeia = "auauau";
+    public void atrapalhaSono() {
         System.out.println(this.onomatopeia);
     }
 }
