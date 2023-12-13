50bb6f8dd49d6b62a27cd5e0f67f0120d2432455
daniel@gmail.com <daniel@gmail.com>
2023-12-12 21:38:20 -0300

    nova atualização

--- heranca00.java
+++ heranca00.java
@@ -2,6 +2,7 @@ public class heranca00 {
     public static void main (String[] args) {
         Cachorro rambo = new Cachorro();
         rambo.fazBarulho();
+        rambo.descrever();
     }
 }
 
@@ -14,10 +15,20 @@ abstract class Animal {
     public void fazBarulho() {
         System.out.println(this.onomatopeia);
     }
+
+    public void descrever() {
+        System.out.println("Raça: " + this. raca);
+        System.out.println("Cor: " + this. cor);
+        System.out.println("Idade: " + this. idade);
+    }
 }
 
 class Cachorro extends Animal {
     public Cachorro() {
         this. onomatopeia = "auau";
+        this. raca = "vira-lata";
+        this. cor = "caramelo";
+        this. idade = 2;
+        
     } 
 }
