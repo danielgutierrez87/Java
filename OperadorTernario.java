0310ca11c8bfe93ea3d02983110b8636fb1dde4c
Daniel Gutierrez <daniell.mx@gmail.com>
2024-02-22 17:57:46 -0300

    novos exercicios

---
+++ OperadorTernario.java
@@ -0,0 +1,15 @@
+public class OperadorTernario {
+    public static void main(String[] args) {
+        int n1, n2, res;
+        n1 = 10;
+        n2 = 5;
+        res = (n1 > n2) ? n1 : n2;
+        System.out.println(res); 
+    }
+}
+/*
+ * () = condição.
+ * ? = se condição for verdadeira mostre n1.
+ * : = senão, mostre n2. 
+ * pode usar equações no lugar de n1 e n2.
+ */
