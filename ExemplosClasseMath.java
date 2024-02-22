802c39f2d3dc0e2947384497e966c06899f8996b
Daniel Gutierrez <daniell.mx@gmail.com>
2024-02-22 16:43:06 -0300

    novo exercicio

---
+++ ExemplosClasseMath.java
@@ -0,0 +1,27 @@
+public class ExemplosClasseMath {
+    
+}
+
+/* ENTRADA DE DADO NUMÉRICO
+ * int num = Integer.parseInt(txtNum.getValue().toString());
+ * 
+ * RESTO DA DIVISÃO POR 2
+ * int r = num % 2;
+ * lblResto.setText(Integer.toString(r));
+ * 
+ * ELEVADO AO CUBO
+ * double c = Math.pow(num, 3);
+ * lblCubo.setText(Double.toString(c));
+ * 
+ * RAIZ QUADRADA
+ * double rq = Math.sqrt(num);
+ * lblRaizQ.setText(String.format("%.2f", rq));
+ * 
+ * RAIZ CUBICA
+ * double rc = Math.cbrt(num);
+ * lblRaizC.setText(String.format("%.2f", rc));
+ * 
+ * VALOR ABSOLUTO
+ * int abs = Math.abs(num);
+ * lblAbs.setText(Integer.toString(abs));
+ */
