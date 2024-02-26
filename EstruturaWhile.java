55a6024083f22a8a7df6ed805ae5d16d4919ea99
Daniel Gutierrez <daniell.mx@gmail.com>
2024-02-26 17:35:26 -0300

    novo exercicio

---
+++ EstruturaWhile.java
@@ -0,0 +1,11 @@
+public class EstruturaWhile {
+    public static void main(String[] args) {
+        
+        int cc = 0;
+        // WHILE FAZ O TESTE LÓGICO NO INÍCIO.
+        while (cc < 4) {
+            System.out.println("Cambalhota " + cc);
+            cc++;
+        }
+    }
+}
