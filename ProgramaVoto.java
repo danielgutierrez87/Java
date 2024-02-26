940efd646f5608d88896198f6a030a6d2f8f9182
Daniel Gutierrez <daniell.mx@gmail.com>
2024-02-26 16:45:55 -0300

    novo exercicio

---
+++ ProgramaVoto.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ProgramaVoto {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Em que ano você nasceu?");
+        int nasc = teclado.nextInt();
+        int idade = 2024 - nasc;
+        System.out.println("Sua idade é " + idade);
+        if (idade < 16) {
+            System.out.println("Não vota!");
+        } else {
+            if ((idade >= 16 && idade < 18) || (idade > 70)) {
+                System.out.println("Voto opcional!");
+            } else {
+                System.out.println("Voto obrigatório!");
+            }
+        }
+    }
+}
