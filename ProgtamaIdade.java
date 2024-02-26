6c64fe700ef31631fc1cf42a804bff2c3f2ced77
Daniel Gutierrez <daniell.mx@gmail.com>
2024-02-26 15:20:14 -0300

    novo exercicio

---
+++ ProgtamaIdade.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ProgtamaIdade {
+    public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+
+        System.out.println("Em que ano você nasceu? ");
+        int nasc = t.nextInt();
+        int i = 2024 - nasc;
+        System.out.println("Sua idade é: " + i);
+
+        if (i>=18) {
+            System.out.println("Maior de idade!");
+        } else {
+            System.out.println("Menor de idade!");
+        }
+    }    
+}
+
