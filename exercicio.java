a42b5f8407a69232e091460aaa5d1621d1247d0e
Daniel Gutierrez <daniell.mx@gmail.com>
2024-03-04 15:41:02 -0300

    novo exercicio

--- exercicio.java
+++ exercicio.java
@@ -1,13 +1,14 @@
 public class exercicio {
     public static void main(String[] args) {
         int total = 0;
-int c[] = new int[13];
-for (int i = 0; i<c.length; i++) {
-	c[i] = i + i;
-}
-for (int i = 0; i<c.length; i++) {
-	total += c[i];
-}
-System.out.println(total);
+        int c[] = new int[13];
+        for (int i = 0; i<c.length; i++) {
+            c[i] = i + i;
+        }
+        for (int i = 0; i<c.length; i++) {
+            total += c[i];
+        }
+
+        System.out.println(total);
     }
 }
